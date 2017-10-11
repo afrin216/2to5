43b30890e10a811f29a57911ede42f121f9aea12
Afrin Subair <afrin.subair@cerner.com>
2017-10-11 17:12:13 -0500

    Add batch3

---
+++ Batch3/README.md
@@ -0,0 +1,13 @@
+Useful utility I use at work. If you have have the need to switch between different java versions, use this script.
+It sets JAVA_HOME variable and prefixes it to global PATH variable for that command window.
+If the folder where this script resides is part of global PATH variable, this can be run as 
+
+```
+WIN_KEY + R (open run command)
+and then 
+
+setjava 16
+setjava 18
+setjava latest 
+
+```
---
+++ Batch3/setjava.bat
@@ -0,0 +1,42 @@
+echo off
+echo ****************************************************************************************************
+if "%1"=="15" goto Java5 
+if "%1"=="5" goto Java5 
+if "%1"=="16" goto Java6 
+if "%1"=="6" (goto Java6)
+if "%1"=="17" goto Java7 
+if "%1"=="7" (goto Java7)
+if "%1"=="18" goto JavaLatest 
+if "%1"=="8" (goto JavaLatest)
+if "%1"=="latest" (goto JavaLatest) else (goto Exit)
+
+:Java5
+echo Setting up the environment for JDK 5
+set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_22
+goto End
+
+:Java6
+echo Setting up the environment for JDK 6
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_45
+goto End
+
+:Java7
+echo Setting up the environment for JDK 7
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_60
+goto End
+
+:JavaLatest
+echo Setting up the environment for JDK 8
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+goto End
+
+
+:End
+PATH=%JAVA_HOME%\bin;%PATH%
+echo.
+echo JAVA_HOME has been set to %JAVA_HOME% and inserted into the PATH.
+ 
+:Exit
+echo.
+java -version
+echo ****************************************************************************************************
